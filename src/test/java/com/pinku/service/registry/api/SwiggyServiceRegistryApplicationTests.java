3161142f15f6af4ce9037f7519d8722001cc5fb5
Pinku <kpinku32@gmail.com>
2024-04-23 17:19:59 +0530

    added src and pom

---
+++ src/test/java/com/pinku/service/registry/api/SwiggyServiceRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pinku.service.registry.api;
+/*
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiggyServiceRegistryApplicationTests {
+
+	@Test
+	@Ignore
+	void contextLoads() {
+	}
+
+}*/
